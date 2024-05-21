53164eef1af1ed597a8971b26067b3677934d507
rushi78441 <145635192+rushi78441@users.noreply.github.com>
2024-05-21 18:32:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# website
+#raportfolio.netlify.app
---
+++ script.js
@@ -0,0 +1,9 @@
+function checkphone(){
+    var phonenumber=document.getElementById("Phone-number").value;
+    if(phonenumber > 9999999999){
+        alert("Phone number is not valid");
+    }
+    else if(isNaN(phonenumber)){
+        alert("Phone number is not valid");
+    }
+}
