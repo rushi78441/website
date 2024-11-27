f3563aeaa7812627d4449ffe99928b12a9c1b0d5
Rushi <rushi78441@gmail.com>
2024-11-27 18:29:23 +0530

    ./

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function checkphone(){
-    var phonenumber=document.getElementById("Phone-number").value;
+    var phonenumber = document.getElementById("Phone-number").value;
     if(phonenumber > 9999999999){
         alert("Phone number is not valid");
     }
